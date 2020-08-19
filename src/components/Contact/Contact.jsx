958800f677a5b4202c0f0579f1dbfee7f87f5b3c
Alexander <jhonalexander9@gmail.com>
2020-08-19 10:49:51 -0500

    set favicon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,6 +11,7 @@ const Contact = () => {
     <div className="Contact">
       <section className="Contact__Profile">
         <h1>Mapa</h1>
+        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d994.1327050628922!2d-74.05152137077489!3d4.677387099787665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a92f350fd3d%3A0xa511f2cbfb341993!2sHercon%20Ltda.!5e0!3m2!1ses-419!2sco!4v1597712832157!5m2!1ses-419!2sco" width="600" height="450" frameBorder="0" style={border:0} allowFullScreen="" aria-hidden="false" tabIndex="0"/> */}
         <div className="Contact__Profile--Info">
           <p>Calle 93A No. 14 - 17 - Of. 308</p>
           <p>Centro 93 Torre Norte - Bogota</p>
@@ -22,7 +23,7 @@ const Contact = () => {
         <p>Celular: 300 774 9639 <FontAwesomeIcon className="faWhatsapp" icon={faWhatsapp} /></p>
         <p>Email: hercon_consultoria@hotmail.com</p>
       </section>
-    </div>
+    </div >
   );
 };
 
